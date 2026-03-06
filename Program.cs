7d6478532afaf6db6d03dc5d98de9c3c69510a44
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 22:01:18 -0600

    Logica Aplicada!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
 ﻿// Escriba un programa que pida números al usuario hasta ingresar 0 y determine cuál fue el número mayor.
+
+int num;
+int mayor = 0;
+
+do
+{
+    Console.WriteLine("Ingrese un número");
+    num = int.Parse(Console.ReadLine());
+    
+    if (num !=0 && num > mayor)
+    {
+        mayor = num;
+    }
+
+
+} while (num != 0);
+
+Console.WriteLine("El número mayor es = "+mayor);
+
